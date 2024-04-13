43d87031b3eb5c983ff5896b81fca21012e3301e
akky25 <ry.akita1991@gmail.com>
2024-04-13 12:16:47 +0900

    Skillsセクションを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { Flex } from "@radix-ui/themes";
 import Hero from "@/components/hero";
 import Home from "@/components/home";
+import Skills from "@/components/skills";
 
 export default function Main() {
   return (
     <main className="flex flex-col lg:flex-row">
       <Hero />
-      <Flex className="grow " height="220rem" direction="column">
+      <Flex className="grow lg:w-3/5" height="220rem" direction="column">
         <Home />
+        <Skills />
       </Flex>
     </main>
   );
