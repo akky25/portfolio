11779a47146b32bcc21b89e4f9ad32b7c9016c0f
akky25 <ry.akita1991@gmail.com>
2024-04-18 00:31:49 +0900

    tmp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
+// import "@radix-ui/themes/styles.css";
 
-import "@radix-ui/themes/components.css";
-import "@radix-ui/themes/tokens/base.css";
-import "@radix-ui/themes/tokens/colors/cyan.css";
-import "@radix-ui/themes/tokens/colors/sage.css";
-import "@radix-ui/themes/utilities.css";
+// import "@radix-ui/themes/components.css";
+// import "@radix-ui/themes/tokens/base.css";
+// import "@radix-ui/themes/tokens/colors/cyan.css";
+// import "@radix-ui/themes/tokens/colors/sage.css";
+// import "@radix-ui/themes/utilities.css";
 
-import "./globals.css";
+// import "./globals.css";
+import "./main.css";
 
 import { Container, Theme } from "@radix-ui/themes";
 import Header from "@/components/header";
